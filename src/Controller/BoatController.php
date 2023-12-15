9ad7125503dee87506d29e00e82e836b0384ca50
severine vanleene <severine.vanleene@gmail.com>
2023-12-15 10:22:22 +0100

    moveboat ()

--- src/Controller/BoatController.php
+++ src/Controller/BoatController.php
@@ -27,4 +27,53 @@ class BoatController extends AbstractController
         
         return $this->redirectToRoute('map');
     }
+      /**
+     * @Route("/boat/direction/{direction}", name="move_direction", requirements={"direction"="[NSWE]"})
+     */
+    public function moveDirection(string $direction, BoatRepository $boatRepository, EntityManagerInterface $entityManager): Response {
+        $boat = $boatRepository->findOneBy([]);
+
+
+
+        if (!$boat) {
+            throw $this->createNotFoundException('No boat found');
+
+        // Get current boat coordinates
+        $coordinates = $boat->getCoordinates();
+        [$x, $y] = $coordinates;
+
+        // Update coordinates based on direction
+        switch ($direction) {
+            case 'N':
+                // Move north (increase Y)
+                $y++;
+                break;
+            case 'S':
+                // Move south (decrease Y)
+                $y--;
+                break;
+            case 'E':
+                // Move east (increase X)
+                $x++;
+                break;
+            case 'W':
+                // Move west (decrease X)
+                $x--;
+                break;
+            // Handle invalid direction (Symfony will generate a 404 error)
+            default:
+                throw $this->createNotFoundException('Invalid direction');
+        }
+
+        // Update Boat coordinates
+        $boat->setCoordinates([$x, $y]);
+
+        
+        $entityManager->flush();
+
+        // Redirect to the map
+        return $this->redirectToRoute('map');
+    }
+    
+    }
 }
