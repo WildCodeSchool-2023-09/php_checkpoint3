7ca8e7dd8f052fe9f2039249c303d29e58114081
yoyodes1000 <yoyodes1000@gmail.com>
2023-12-15 10:26:48 +0100

    first atomic commit, the boat is moving

--- src/Controller/BoatController.php
+++ src/Controller/BoatController.php
@@ -13,18 +13,48 @@ class BoatController extends AbstractController
 {
     #[Route('/move/{x<\d+>}/{y<\d+>}', name: 'moveBoat')]
     public function moveBoat(
-        int $x,
-        int $y,
-        BoatRepository $boatRepository,
+        int                    $x,
+        int                    $y,
+        BoatRepository         $boatRepository,
         EntityManagerInterface $entityManager
-    ): Response {
+    ): Response
+    {
         $boat = $boatRepository->findOneBy([]);
-        
+
         $boat->setCoordX($x);
         $boat->setCoordY($y);
 
         $entityManager->flush();
-        
+
         return $this->redirectToRoute('map');
     }
+
+    #[Route('/direction/{direction}', name: 'moveDirection')]
+    public function moveDirection(string $direction,
+                                  BoatRepository $boatRepository,
+                                  EntityManagerInterface $entityManager,): Response
+    {
+        $boat = $boatRepository->findOneBy([]);
+
+        $x = $boat->getCoordX();
+        $y = $boat->getCoordY();
+        switch($direction) {
+            case 'N' :
+                $boat->setCoordY($y - 1);
+                break;
+            case 'S' :
+                $boat->setCoordY($y + 1);
+                break;
+            case 'W' :
+                $boat->setCoordX($x - 1);
+                break;
+            case 'E' :
+                $boat->setCoordX($x + 1);
+                break;
+        }
+        $entityManager->persist($boat);
+        $entityManager->flush();
+        return $this->redirectToRoute('map');
+    }
+
 }
