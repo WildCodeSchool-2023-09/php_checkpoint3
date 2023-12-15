44219f12a6245671fd96b723ad8e4bcf5c1e2cf2
Julie <raymondjulie@proton.me>
2023-12-15 10:06:22 +0100

    exercice 10 done

--- src/Controller/MapController.php
+++ src/Controller/MapController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Service\MapManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -26,5 +28,29 @@ class MapController extends AbstractController
             'map'  => $map ?? [],
             'boat' => $boat,
         ]);
+    }
+
+    #[Route('/start', name: 'start')]
+    public function start(BoatRepository $boatRepository, MapManager $mapManager, TileRepository $tileRepository, EntityManagerInterface $em) :Response
+    {
+        $boat = $boatRepository->findOneBy([]);
+        $boat->setCoordX(0);
+        $boat->setCoordY(0);
+
+        $previousTreasure = $tileRepository->findOneBy([
+            'hasTreasure' => true
+        ]);
+
+        if ($previousTreasure != null) {
+            $previousTreasure->setHasTreasure(false);
+        }
+
+        $randomIsland = $mapManager->getRandomIsland();
+        $randomIsland->setHasTreasure(true);
+
+        $em->persist($boat);
+        $em->flush();
+
+        return $this->redirectToRoute('map');
     }
 }
