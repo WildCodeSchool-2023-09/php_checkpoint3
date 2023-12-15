bfc8e0604f515c5d26a3fe72e5041857c5d5bccd
Jojopok <zaiajonathan@gmail.com>
2023-12-15 11:08:23 +0100

    5

--- src/Controller/BoatController.php
+++ src/Controller/BoatController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 use App\Repository\BoatRepository;
+use App\Repository\TileRepository;
+use App\Service\MapManager;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -11,20 +13,53 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route('/boat')]
 class BoatController extends AbstractController
 {
+
+    private $mapManager;
+
+    public function __construct(MapManager $mapManager)
+    {
+        $this->mapManager = $mapManager;
+    }
     #[Route('/move/{x<\d+>}/{y<\d+>}', name: 'moveBoat')]
     public function moveBoat(
-        int $x,
-        int $y,
-        BoatRepository $boatRepository,
+        int                    $x,
+        int                    $y,
+        BoatRepository         $boatRepository,
         EntityManagerInterface $entityManager
-    ): Response {
+    ): Response
+    {
         $boat = $boatRepository->findOneBy([]);
-        
+
         $boat->setCoordX($x);
         $boat->setCoordY($y);
 
         $entityManager->flush();
-        
+
+        return $this->redirectToRoute('map');
+    }
+
+    #[Route('/direction/{direction}', name: 'moveDirection', requirements: ['direction' => '[NSEW]'])]
+    public function moveDirection(
+        BoatRepository         $boatRepository,
+        EntityManagerInterface $entityManager,
+        string                 $direction,
+
+    ): Response
+    {
+        $boat = $boatRepository->findOneBy([]);
+
+        if ($direction === 'N') {
+            $boat->setCoordY($boat->getCoordY() - 1);
+        } elseif ($direction === 'S') {
+            $boat->setCoordY($boat->getCoordY() + 1);
+        } elseif ($direction === 'E') {
+            $boat->setCoordX($boat->getCoordX() + 1);
+        } elseif ($direction === 'W') {
+            $boat->setCoordX($boat->getCoordX() -1 );
+        }
+        if ($this->mapManager->tileExists($boat->getCoordX(), $boat->getCoordY())) {
+            $entityManager->flush();
+        }
         return $this->redirectToRoute('map');
     }
 }
