38cb54e38288f087fe74b7b84d74cc9b5579de3b
Julie <raymondjulie@proton.me>
2023-12-15 09:56:21 +0100

    exercice 9 done

--- src/Service/MapManager.php
+++ src/Service/MapManager.php
@@ -2,6 +2,7 @@
 
 namespace App\Service;
 
+use App\Entity\Tile;
 use App\Repository\TileRepository;
 
 class MapManager
@@ -20,5 +21,15 @@ class MapManager
             'coordY' => $coordY,
         ]);
         return $tile != null;
+    }
+
+    public function getRandomIsland(): Tile
+    {
+        $islands = $this->tileRepository->findBy([
+            'type' => 'island'
+        ]);
+
+        $islandKey = array_rand($islands, 1);
+        return $islands[$islandKey];
     }
 }
