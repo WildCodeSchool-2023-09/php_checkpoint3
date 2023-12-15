6327e347d63736fd709fc8ac4b8f09354266dff1
Julien Ginestal <144034157+Dju0@users.noreply.github.com>
2023-12-15 10:42:22 +0100

    méthone Start faite

--- src/Controller/MapController.php
+++ src/Controller/MapController.php
@@ -4,10 +4,13 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Tile;
 use App\Repository\BoatRepository;
 use App\Repository\TileRepository;
+use App\Service\MapManager;
+use Doctrine\ORM\EntityManagerInterface;
 
 class MapController extends AbstractController
 {
@@ -26,5 +29,36 @@ class MapController extends AbstractController
             'map'  => $map ?? [],
             'boat' => $boat,
         ]);
+    }
+
+    #[Route('/start', name: 'start_game')]
+    public function start(BoatRepository $boatRepository, MapManager $mapManager, EntityManagerInterface $entityManager, TileRepository $tileRepository): RedirectResponse
+    {
+        // Réinitialiser les coordonnées du bateau
+        $boat = $boatRepository->findOneBy([]);
+        if ($boat) {
+            $boat->setCoordX(0);
+            $boat->setCoordY(0);
+            $entityManager->persist($boat);
+        }
+
+        // Réinitialiser le trésor sur les tuiles
+        $tilesWithTreasure = $tileRepository->findBy(['hasTreasure' => true]);
+        foreach ($tilesWithTreasure as $tile) {
+            $tile->setHasTreasure(false);
+            $entityManager->persist($tile);
+        }
+
+        // Placer le trésor sur une île aléatoire
+        $randomIsland = $mapManager->getRandomIsland();
+        if ($randomIsland) {
+            $randomIsland->setHasTreasure(true);
+            $entityManager->persist($randomIsland);
+        }
+
+        $entityManager->flush();
+
+        // Rediriger vers /map
+        return $this->redirectToRoute('map');
     }
 }
